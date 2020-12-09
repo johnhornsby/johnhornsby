5216a71eed49a4e7737ccb9d29c9c6e530541576
John Hornsby <johnhornsby@johns-imac.lan>
2020-12-09 11:49:49 +0000

    GTM addition

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import getConfig from 'next/config';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -28,6 +29,7 @@ export default class MyDocument extends Document {
   }
 
   render() {
+    const { publicRuntimeConfig } = getConfig();
     return (
       <Html>
         <Head>
@@ -35,8 +37,26 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
           />
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${publicRuntimeConfig.gtm}');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+              dangerouslySetInnerHTML={{
+                  __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${publicRuntimeConfig.gtm}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              }}
+          />
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     // Will be available on both server and client
     apolloClientUri: process.env.APOLLO_CLIENT_URI,
+    gtm: process.env.GTM,
   },
   serverRuntimeConfig: {
     apolloClientUri: process.env.APOLLO_SERVER_URI,
